0eeba9e5c9cb2843d2bc64c0389c80c827c00b48
Marcin <36981989+MarcinZemlok@users.noreply.github.com>
2020-11-24 08:48:24 +0100

    Done: Contact page done
    TODO: Social links

--- js/index.js
+++ js/index.js
@@ -11,3 +11,28 @@ class Background {
 }
 
 background = new Background();
+
+class MailForm {
+    constructor() {
+        this.form = document.getElementById("contact-form");
+        this.form.onsubmit = this.handleSubmit;
+    }
+
+    handleSubmit = (evt) => {
+        const senderName = this.form[0].value;
+        const senderEmail = this.form[1].value;
+        const senderOrganization = this.form[2].value;
+        const senderMessage = this.form[3].value;
+
+        const encodedURI = encodeURI(`mailto:marcinzemlok@gmail.com?subject=Portfolio contact&body=Sender name: ${senderName}\nSender email: ${senderEmail}\nSender organization: ${senderOrganization}\nMessage:\n${senderMessage}`);
+
+        window.open(
+            encodedURI,
+            "_blank"
+        );
+
+        evt.preventDefault;
+    }
+}
+
+mailForm = new MailForm();
